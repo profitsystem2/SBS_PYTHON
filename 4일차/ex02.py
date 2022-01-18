db385ad23eaa2bb38aed9c1689ee6e9b844bbaf9
profitsystem2 <yj020910@naver.com>
2022-01-18 15:54:36 +0900

    4일차

---
+++ 4일차/ex02.py
@@ -0,0 +1,15 @@
+name ='박면제'
+print('내 이름은 %s입니다.'%name)
+
+height=211.3
+print('내 키는 %.1f cm입니다.'%height)
+
+weight=139.7
+print('내 몸무게는 %.1f kg입니다.'%weight)
+
+hm=height/100
+bmi=weight/hm**2
+print('내 bmi는 %.2f입니다.'%bmi)
+year,month,day=2022,1,18
+print('내 생일은 %d년 %d월 %d일입니다.'%(year,month,day))
+
